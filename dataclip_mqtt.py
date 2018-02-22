eb6e422c93e8b294682b1d4ea14fcc7218ba5968
Steve Pomeroy <steve@staticfree.info>
2018-02-22 16:36:39 -0500

    Initial commit

---
+++ dataclip_mqtt.py
@@ -0,0 +1,43 @@
+"""Dataclip -> MQTT"""
+
+import json
+import time
+import sys
+from threading import Thread
+
+from mqtt_base import MQTTBase
+from eventplayback import EventPlayback, PlaybackTarget
+from dataclip_source import DataClipSource
+
+
+class MQTTTarget(MQTTBase, PlaybackTarget):
+    def __init__(self, config_file):
+        MQTTBase.__init__(self, config_file=config_file)
+        self._topic = 'iot/order'
+
+    def on_connect(self, client, userdata, flags, conn_result):
+        print("Connected to MQTT server.")
+
+    def on_event(self, event):
+        self.mqtt.publish(self._topic, json.dumps(event))
+
+
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: {} MQTT_CONFIG_FILE DATA_CLIP_CONFIG_FILE".format(sys.argv[0]))
+        sys.exit(1)
+
+    target = MQTTTarget(sys.argv[1])
+    target.connect()
+    mqtt_thread = Thread(target=target.loop_forever)
+    mqtt_thread.start()
+    source = DataClipSource(sys.argv[2])
+    event_playback = EventPlayback(target, source, 60, 165)
+    event_playback.start()
+    while True:
+        event_playback.tick()
+        time.sleep(0.001)
+
+
+if __name__ == "__main__":
+    main()
---
+++ eventplayback.py
@@ -0,0 +1,95 @@
+"""Event playback"""
+
+from __future__ import print_function
+import datetime
+import time
+import threading
+import queue
+import pytz
+
+
+class EventSource(object):
+    """A time-windowed source of events"""
+
+    def events(self, start, end):
+        """Return a list of events within the time window"""
+        pass
+
+    def get_date(self, event):
+        """Gets the date for the given event"""
+        pass
+
+
+class PlaybackTarget(object):
+    def on_event(self, event):
+        raise Exception("must implement this method")
+
+
+class DebugTarget(PlaybackTarget):
+    def on_event(self, event):
+        print("Event: {}".format(event))
+
+
+class EventPlayback(object):
+    """Replays the events from the given EventSource to the PlaybackTarget"""
+    events_queue = queue.Queue()
+    load_lock = threading.Event()
+    event = None
+    offset_event_time = None
+
+    def __init__(self, target, event_source, load_window, playback_offset, min_buffer_size=40):
+        """
+        load_window in seconds
+        playback_offset in seconds
+        """
+        self.target = target
+        self.event_source = event_source
+        self.load_window = datetime.timedelta(seconds=load_window)
+        self.playback_offset = datetime.timedelta(seconds=playback_offset)
+        self.min_buffer_size = min_buffer_size
+
+    def to_microseconds(self, atime):
+        return int(time.mktime(atime.timetuple()) * 1000 + atime.microsecond/1000)
+
+    def load_events(self):
+        last_load = None
+        while True:
+            self.load_lock.wait()
+            print("Loading events...")
+
+            interval = int(self.load_window.total_seconds() * 1000)
+            end = self.to_microseconds(datetime.datetime.now() - self.playback_offset) + interval
+            if last_load:
+                delay = max(0, (last_load + interval/2) - end)
+                if delay:
+                    print("Reloading too fast. Waiting...")
+                time.sleep(delay / 1000)
+                end = self.to_microseconds(datetime.datetime.now() - self.playback_offset) + interval
+            else:
+                last_load = end - interval
+            events = self.event_source.events(last_load, end)
+            for event in events:
+                self.events_queue.put(event)
+            print("Loaded {:d} events.".format(len(events)))
+            last_load = end
+            self.load_lock.clear()
+
+    def start(self):
+        thread = threading.Thread(target=self.load_events)
+        thread.daemon = True
+        thread.start()
+
+    def tick(self):
+        # if the buffer is low, fill it up
+        if self.events_queue.qsize() < self.min_buffer_size and not self.load_lock.is_set():
+            self.load_lock.set()
+        if not self.event:
+            try:
+                self.event = self.events_queue.get(block=False)
+                self.offset_event_time = self.event_source.get_date(self.event) + self.playback_offset
+            except queue.Empty:
+                self.event = None
+        if self.event and self.offset_event_time <= datetime.datetime.now(tz=pytz.utc):
+            print('.', end='', flush=True)
+            self.target.on_event(self.event)
+            self.event = None
